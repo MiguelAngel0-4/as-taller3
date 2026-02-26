0ddbabf8c6a98d76d4e63fcdf0291cdf01ab2802
Miguel A Fernandez <migue2801224@gmail.com>
2026-02-25 23:44:11 -0500

    creación archivo JavaScrips

---
+++ webapp/static/js/main.js
@@ -0,0 +1,20 @@
+// TODO: Agregar funcionalidad JavaScript
+
+document.addEventListener('DOMContentLoaded', function() {
+    // TODO: Inicializar componentes cuando cargue la página
+});
+
+// TODO: Función para agregar productos al carrito con AJAX
+function addToCart(productId) {
+    // TODO: Implementar agregado al carrito
+}
+
+// TODO: Función para actualizar cantidad en el carrito
+function updateCartQuantity(itemId, quantity) {
+    // TODO: Implementar actualización de cantidad
+}
+
+// TODO: Función para remover items del carrito
+function removeFromCart(itemId) {
+    // TODO: Implementar remoción de items
+}

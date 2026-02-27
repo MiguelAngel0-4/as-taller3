42b69539eeadfc2083453895584bdae2d798378d
Miguel A Fernandez <migue2801224@gmail.com>
2026-02-26 22:46:36 -0500

    implementación de product completo

--- api/models/product.py
+++ api/models/product.py
@@ -13,7 +13,15 @@ class Product(Base):
     # stock = Column(...)
     # image_url = Column(...)
     # created_at = Column(...)
-    
+    id          = Column(Integer, primary_key=True, index=True)
+    name        = Column(String(100), nullable=False)
+    description = Column(Text)
+    price       = Column(Numeric(10, 2), nullable=False, default=0.00)
+    stock       = Column(Integer, nullable=False, default=0)
+    image_url   = Column(Text)
+    created_at  = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+
     def __repr__(self):
         # TODO: Implementar representación del objeto
-        pass
+        return f"<Product id={self.id} name={self.name!r} price={self.price}>"
